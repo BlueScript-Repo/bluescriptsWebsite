b94eb74a29b465da89a3460697c9f7ca937e9c97
BlueScript-Repo <repo@bluescripts.in>
2021-08-27 12:42:12 +0530

    commit

--- src/main/java/com/bluescripts/website/controller/LoginController.java
+++ src/main/java/com/bluescripts/website/controller/LoginController.java
@@ -1,3 +1,7 @@
 package com.bluescripts.website.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
 public class LoginController {}
